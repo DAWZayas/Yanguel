4ece841f4f423207a12a22fb49903c33025d7ea1
Raúl Pavón <raulpavonfp@gmail.com>
2017-11-02 11:52:45 +0100

    WIP Product component

---
+++ components/common/Product.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class = "jumbotron">
+    <div class="container">
+       <img src="coche.jpg">
+      <p class="text title">Nombre de producto</p>
+      <div class="prices">
+          <p class="text offer">Oferta</p>
+          <p class="text price">Precio</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'product',
+    components: {
+    },
+    props: [],
+  }
+</script>
+<style lang="scss">
+  @import "../../assets/styles/base/colors";
+  @import "../../assets/styles/base/variables";
+  @import "../../assets/styles/vendors/flex";
+  @import "../../assets/styles/vendors/bootstrap/functions";
+  @import "../../assets/styles/vendors/bootstrap/variables";
+  @import "../../assets/styles/vendors/bootstrap/mixins";
+
+
+</style>
