bf1a02bd0dba8b5c070c840b997b962df73d4fee
Raúl Pavón <raulpavonfp@gmail.com>
2017-11-13 13:57:43 +0100

    [WIP] trying to add a Modal into Product component

--- components/product/Product.vue
+++ components/product/Product.vue
@@ -8,7 +8,27 @@
       </div>
       <div class = "button-div btn-group">
       <button class = "btn btn-primary" @click="onAdd"> <i class="material-icons">add_shopping_cart</i></button>
-      <button class = "btn btn-default" > Details</button>
+      <button class = "btn btn-default" data-toggle="modal" data-target = "#productModal" > Details</button>
+      </div>
+      <div class="modal fade" id="productModal" tabindex="1" role="dialog" aria-labelledby="productModal" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="productModalLabel">{{product.name}}</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              Descripción: {{product.description}} <br>
+              Precio: {{product.price}}
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 </template>
