66539bd67247b0de9de5dfba15d973c9bb82401f
Raúl Pavón <raulpavonfp@gmail.com>
2018-02-22 12:56:42 +0100

    [WIP] Minor changes

--- components/common/HeaderComponent.vue
+++ components/common/HeaderComponent.vue
@@ -13,12 +13,6 @@
       <el-menu-item index="1-3" v-if="user && user.name" @click="onLogout"><nuxt-link to="/"><i class="material-icons">account_box</i>Logout</nuxt-link></el-menu-item>
     </el-submenu>
     <el-menu-item index="2"><i class="material-icons" @click="showSearchBar">search</i></el-menu-item>
-    <el-submenu index="3">
-      <template slot="title">Filter Tags</template>
-      <el-menu-item index="3-1">Otros</el-menu-item>
-      <el-menu-item index="3-2">Informática</el-menu-item>
-      <el-menu-item index="3-3">Electrónica</el-menu-item>
-    </el-submenu>
   </el-menu>
 </template>
 
@@ -42,7 +36,7 @@
       onSuccessLogout () {
         this.$notify.success({
           title: 'Bye!',
-          message: 'Successfuly logged out, we hope to see you soon!.',
+          message: 'Successfuly logged out, we hope to see you soon!',
           position: 'bottom'
         })
       }
